89911bfef6812c89e8bba2af6f3f445de936ee48
amineamanzou <amineamanzou@gmail.com>
2013-01-04 17:09:37 +0100

    Adding the bases
    Adding makefiles and folder structure

---
+++ TCP-FLT/client.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (void){
+	printf("I am the client\n");
+	return EXIT_SUCCESS;
+}
---
+++ UDP-FLT/server.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (void){
+	printf("I am the server\n");
+	return EXIT_SUCCESS;
+}
